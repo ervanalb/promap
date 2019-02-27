27ae7d222ca2e6d980d86f2009fc76f2e2ee503f
Eric Van Albert <eric@van.al>
2019-02-26 22:33:02 -0500

    add dependencies

--- setup.py
+++ setup.py
@@ -27,4 +27,8 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    install_requires=[
+        'numpy',
+        'scipy',
+    ],
 )

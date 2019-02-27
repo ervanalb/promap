a4c3954f4433786d4d54f5b61aaa68990592c06e
Eric Van Albert <eric@van.al>
2019-02-26 22:27:10 -0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+long_description = """`promap` is a command-line tool that you can use
+in conjunction with your digital video workflow
+to do projection mapping.
+
+`promap` uses a projector and a single camera
+to compute the physical scene as viewed from the projector,
+as well as a coarse disparity (depth) map.
+
+You can import the maps that it generates straight into your [digital video workflow](https://radiance.video),
+or you can post-process them into masks and uvmaps first using image processing tools.
+"""
+
+setuptools.setup(
+    name="promap",
+    version="0.0.1",
+    author="Eric Van Albert",
+    author_email="eric@van.al",
+    description="Projection mapping pipeline",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ervanalb/promap",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
